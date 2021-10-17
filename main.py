295c48e2b44b8d780320527535b3c7fa9b183e35
handyzulkarnain <zulkarnainhandy@gmail.com>
2021-10-17 14:15:55 +0700

    Commit 2: Done authentication

--- main.py
+++ main.py
@@ -1,17 +1,92 @@
 # Nama  : Handy Zulkarnain
 # NIM   : 18219060
 
-from os import name
+############ IMPORTS AND INITIAL CONFIGURE ############
 from fastapi import FastAPI
 import json
 
 from fastapi.exceptions import HTTPException
 
+import time
+from typing import Dict
+import jwt
+from decouple import config
+
 with open("menu.json", "r") as read_file:
     data = json.load(read_file)
+with open("users.json", "r") as read_users_file:
+    data_users = json.load(read_users_file)
 
 app = FastAPI()
 
+
+############ AUTHENTICATIONS ############
+JWT_SECRET = config("secret")
+JWT_ALGORITHM = config("algorithm")
+
+def res_access_token(token: str):
+    return {
+        "access_token": token
+    }
+
+def createJWT(user_id: str) -> Dict[str, str]:
+    body = {
+        "user_id": user_id,
+        "expiry_time": time.time() + 1200
+    }
+    token = jwt.encode(body, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return res_access_token(token)
+
+def decodeJWT(token: str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        if (decoded_token["expiry_time"] >= time.time()):
+            return decoded_token
+        else:
+            None
+    except:
+        return {}
+
+
+############ HANDLE USERS ############
+def check_user(username: str):
+    for user in data_users['users']:
+        if username == user['username']:
+            return True
+    return False
+
+def check_password(password: str):
+    for user in data_users['users']:
+        if password == user['password']:
+            return True
+    return False
+
+@app.post("/user/login")
+def login_user(username: str, password: str):
+    for user in data_users['users']:
+        if (check_user(username)) and (check_password(password)):
+            return createJWT(user['username'])
+    raise HTTPException (
+        status_code=500, detail="Wrong credentials!"
+    )
+
+@app.post("/user/register")
+def create_user(username: str, password: str):
+    if check_user(username):
+        raise HTTPException(
+            status_code=500, detail="Username has taken! Please proceed to login!"
+        )
+    else: 
+        new_data = {"username":username, "password":password}
+        data_users['users'].append(dict(new_data))
+        read_users_file.close()
+        with open("users.json", "w") as write_file:
+            json.dump(data_users, write_file, indent=4)
+        write_file.close()
+        return createJWT(new_data['username'])
+
+
+############ CRUD OPERATIONS ############
 @app.get("/")
 def root():
     return "Anda sedang berada di halaman awal. Silahkan tambahkan /docs pada akhir url."
